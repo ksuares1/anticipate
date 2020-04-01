d47e09019afeee7144dbf7f6d2ba7f4f0e50624d
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-03-31 23:26:53 -0400

    added nav tag  to app.js

--- anticipate/src/App.js
+++ anticipate/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        <p>Logo</p>
+        <p>About</p>
+        <p>Contact</p>
+        <p>Login</p>
+      </nav>
+
+
     </div>
   );
 }
