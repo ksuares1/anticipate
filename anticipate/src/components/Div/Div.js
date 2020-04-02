0de85806fda63ee3b7b2c53b7cadbb874cb78385
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-02 09:52:15 -0400

    updated div.js button

--- anticipate/src/components/Div/Div.js
+++ anticipate/src/components/Div/Div.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 const Div = () => {
     return (
-        <div>
-            <h1>Anticipate assists nurses and medical professionals with their interactions with patients.</h1>
-            <button> </button>
-        </div>
+        <main>
+            <br>
+            </br>
+            <div>
+                <h1>Anticipate assists nurses and medical professionals with their interactions with patients.</h1>
+                <Button variant="outline-dark">Find Out More</Button>{' '}
+            </div>
+        </main>
 
     )
 }
