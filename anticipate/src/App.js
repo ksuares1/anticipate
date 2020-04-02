6a845f0876f764ba5e2008cdbe9cf52096139cc4
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-02 00:51:20 -0400

    changed nav link href to about

--- anticipate/src/App.js
+++ anticipate/src/App.js
@@ -21,7 +21,7 @@ function App() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">About</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
       <Nav.Link href="#link">Family</Nav.Link>
       <NavDropdown title="Nurse" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Patient</NavDropdown.Item>
