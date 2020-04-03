c74f4657e0756daf040571068dde857645aafb91
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-02 22:31:55 -0400

    added buttons and texts to cards

--- anticipate/src/components/Cards/Cards.js
+++ anticipate/src/components/Cards/Cards.js
@@ -1,32 +1,32 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
-
+import Button from 'react-bootstrap/Button';
 const CardComponent = () => {
     return (
         <CardDeck>
-        <Card>
-            <Card.Img variant="top" src={require("../../assets/img/nurse-patient.jpg")} />
-            <Card.Body>
-                <Card.Title>Nurse</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-      </Card.Text>
-            </Card.Body>
-        </Card>
-        <Card>
-            <Card.Img variant="top" src={require("../../assets/img/family.jpg")}  />
-            <Card.Body>
-                <Card.Title>Family</Card.Title>
-                <Card.Text>
-                    This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-                </Card.Text>
-            </Card.Body>
-            
-        </Card>
-  </CardDeck>
+            <Card>
+                <Card.Img variant="top" src={require("../../assets/img/nurse-patient.jpg")} />
+                <Card.Body>
+                    <Card.Title>Nurse</Card.Title>
+                    <Card.Text>
+                        <p> Seamlessly check-in with your patients.</p>
+                        <Button variant="outline-dark">Check-In</Button>{' '}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src={require("../../assets/img/family.jpg")} />
+                <Card.Body>
+                    <Card.Title>Family</Card.Title>
+                    <Card.Text>
+                        <p>Keep track of your family members medications.</p>
+                        <Button variant="outline-dark">Login</Button>{' '}
+                    </Card.Text>
+                </Card.Body>
+
+            </Card>
+        </CardDeck>
     )
 }
 export default CardComponent;
