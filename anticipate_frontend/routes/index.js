69b45062b9795fac8e2379dacab19fe43be96ec6
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-09 22:35:57 -0400

    added api folder and code for user.js

--- anticipate_frontend/routes/index.js
+++ anticipate_frontend/routes/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 const userController = require("../controllers/userController.js");
 
 // API Routes
