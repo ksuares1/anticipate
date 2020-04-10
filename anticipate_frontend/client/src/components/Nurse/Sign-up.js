1965179726d25da62c83e343814944e938e055c0
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-09 21:31:40 -0400

    added code to routes controllers and localsstrategy

--- anticipate_frontend/client/src/components/Nurse/Sign-up.js
+++ anticipate_frontend/client/src/components/Nurse/Sign-up.js
@@ -1,22 +1,41 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import axios from "axios";
+
+class SignupComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Username:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                
+                <label>
+                    Password:
+              <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
 
 
-
-SignupComponent= () => {
-    return (
-        <Form>
-            <Form.Group controlId="formGroupEmail">
-                <Form.Label className="text-align:center">Usernmae</Form.Label>
-                <Form.Control type="username" placeholder="username" />
-            </Form.Group>
-            <Form.Group controlId="formGroupPassword">
-                <Form.Label className="text-align:center">Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-        </Form>
-
-    )
-}
 export default SignupComponent;
