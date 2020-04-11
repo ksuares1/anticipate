514df85bcd1ba01825e2c9527416179b26636eea
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-10 23:30:45 -0400

    added code to nurse.js

--- anticipate_frontend/client/src/pages/Nurse.js
+++ anticipate_frontend/client/src/pages/Nurse.js
@@ -1,2 +1,18 @@
 import React from "react";
+import Nurse from "../components/Nurse/Nursediv.js";
+import LoginComponent from "../components/Nurse/Login.js";
 
+
+function showNurse() {
+    return (
+        <div>
+        <Nurse/>
+        <div>
+            <LoginComponent/>
+            </div>
+        </div>
+    
+    )
+}
+
+export default showNurse;
