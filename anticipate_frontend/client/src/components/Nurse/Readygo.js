24cdc12351fac8e8b0eee484b925dfbcad050d96
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-15 21:01:03 -0400

    Added background color to readygo component

--- anticipate_frontend/client/src/components/Nurse/Readygo.js
+++ anticipate_frontend/client/src/components/Nurse/Readygo.js
@@ -5,16 +5,19 @@ import Microphone from "../../pages/Mic.js";
 
 function showMic() {
     return (
-        <div className="mic-page">
+        <main className="readygo">
+        <div className="mic-page" >
             <Jumbotron fluid>
                 <Container>
                     <h1>Ready Go</h1>
                     <p>Staying connected with patients is important throughout the duration of their hospital stay and out-patient care.
-                      Ready Go helps nurses to quickly send text messages to patients, so they can care for mutiple patients at a time. </p>
+                     Ready Go helps nurses to quickly send text messages to patients, so they can care for mutiple patients at a time. </p>
                 </Container>
             </Jumbotron>
             <Microphone />
         </div>
+      
+        </main>
     )
 }
 
