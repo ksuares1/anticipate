2c47e72f653c5aefe47adaf0b634651a791c4036
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-15 20:30:55 -0400

    added jumbotron to readygo page

--- anticipate_frontend/client/src/components/Nurse/Readygo.js
+++ anticipate_frontend/client/src/components/Nurse/Readygo.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 import Microphone from "../../pages/Mic.js";
 
-function showMic(){
-    return(
-        <Microphone/>
+function showMic() {
+    return (
+        <div className="mic-page">
+            <Jumbotron fluid>
+                <Container>
+                    <h1>Ready Go</h1>
+                    <p>Staying connected with patients is important throughout the duration of their hospital stay and out-patient care.
+                      Ready Go helps nurses to quickly send text messages to patients, so they can care for mutiple patients at a time. </p>
+                </Container>
+            </Jumbotron>
+            <Microphone />
+        </div>
     )
 }
 
