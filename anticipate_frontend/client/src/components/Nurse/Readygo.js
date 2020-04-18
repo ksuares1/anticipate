b3d4c9635704d4cc67c07a3988c31721581c5bab
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-17 22:38:01 -0400

    added coming soon jumbotron to readygo page

--- anticipate_frontend/client/src/components/Nurse/Readygo.js
+++ anticipate_frontend/client/src/components/Nurse/Readygo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 //  import Microphone from "../../pages/Mic.js";
 
 function showMic() {
@@ -10,8 +11,20 @@ function showMic() {
                 <Container>
                     <h1>Ready Go</h1>
                     <p>Staying connected with patients is important throughout the duration of their hospital stay and out-patient care.
-                     Ready Go helps nurses to quickly send text messages to patients, so they can care for mutiple patients at a time. </p>
+                     Anticipate's voice recording system called Ready Go helps nurses to quickly send text messages to patients, so they can care for mutiple patients at a time. </p>
                 </Container>
+            </Jumbotron>
+            <br>
+            </br>
+            <Jumbotron className="coming-soon">
+                <h1>Coming Soon</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+  </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
             </Jumbotron>
         </main>
     )
