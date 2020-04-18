0067dad1ca80deb5f09be7a33d6cb5fbfcae6bd0
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-17 20:36:16 -0400

    updated the cards

--- anticipate_frontend/client/src/components/Cards/Cards.js
+++ anticipate_frontend/client/src/components/Cards/Cards.js
@@ -7,16 +7,16 @@ import Button from 'react-bootstrap/Button';
 const CardComponent = () => {
     return (
         <CardDeck>
-            <Card className="first-card text-center">
-                <Card.Img  className="nurse-image" variant="top" src={require("../../assets/img/nurse-lab.jpg")} />
-                <Card.Body>
-                    <Card.Title className="nurse-card">Nurse</Card.Title>
-                    <Card.Text>
-                        <p className="text-center"> Seamlessly check-in with your patients.</p>
-                        <Button bstyle="default"  className="login-button" variant="outline-dark">Login</Button>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+             <Card className="first-card text-center">
+            <Card.Img className="nurse-image" variant="top" src={require("../../assets/img/nurse-lab.jpg")} />
+            <Card.Body>
+                <Card.Title className="nurse-card">Nurse</Card.Title>
+                <Card.Text>
+                    <p className="text-center"> Seamlessly check-in with your patients.</p>
+                    <Button bstyle="default" className="login-button" variant="outline-dark">Login</Button>
+                </Card.Text>
+            </Card.Body>
+             </Card> 
             <Card className="second-card text-center" >
                 <Card.Img className="patient-image" variant="top" src={require("../../assets/img/nurse-patient.jpg")} />
                 <Card.Body>
@@ -24,11 +24,11 @@ const CardComponent = () => {
                     <Card.Text>
                         <p className="text-center"> Nurses can quickly and securely see  the latest medicial updates on their patients.</p>
                     </Card.Text>
-                    <Button  variant="outline-dark">Track</Button>
+                    <Button variant="outline-dark">Track</Button>
                 </Card.Body>
 
             </Card>
-        </CardDeck>
+        </CardDeck> 
     )
 }
 export default CardComponent;
