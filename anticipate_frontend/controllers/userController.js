172486b92441b9d4325668711a4c25d3a8661a29
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-04-21 21:35:23 -0400

    added code to usercontroller

--- anticipate_frontend/controllers/userController.js
+++ anticipate_frontend/controllers/userController.js
@@ -19,46 +19,53 @@ module.exports = {
         const { username, password } = req.body
         console.log("username, password", username, password);
         // ADD VALIDATION
-        db.User.find( req.body, (err, user) => {
-            if (err) {
-                console.log('User.js post error: ', err)
-            } else if (user) {
-                res.json({
-                    error: `Sorry, already a user with the username: ${username}`
-                })
-            }
-            else {
-                const newUser = new User({
-                    username: username,
-                    password: password
-                })
-                newUser.save((err, savedUser) => {
-                    if (err) return res.json(err)
-                    res.json(savedUser)
-                })
-            }
-        })
+        // db.User.find(req.body, (err, user) => {
+        //     if (err) {
+        //         console.log('User.js post error: ', err)
+        //     } else if (user) {
+        //         res.json({
+        //             error: `Sorry, already a user with the username: ${username}`
+        //         })
+        //     }
+        //     else {
+        //         const newUser = new User({
+        //             username: username,
+        //             password: password
+        //         })
+        //         newUser.save((err, savedUser) => {
+        //             if (err) return res.json(err)
+        //             res.json(savedUser)
+        //         })
+        //     }
+        // })
         passport.authenticate('local'),
-            db.User
-                .find(req.body)
-                .then(dbModel => {
-                    if (!dbModel.checkUsername(username)) {
-                        return done(null, false, { message: 'Incorrect username' })
-                    }
+            console.log("req.body", req.body)
+        db.User
+            .find(req.body)
+            .then(dbModel => {
+                console.log("dbModel", dbModel);
+                if (dbModel === 0) {
+                    return res.send("user not found");
+                }
+
+                if (dbModel.length !== 0) {
+                    // if (!dbModel.checkUsername(username)) {
+                    //     return done(null, false, { message: 'Incorrect username' })
+                    // }
                     if (!dbModel.checkPassword(password)) {
                         return done(null, false, { message: 'Incorrect password' })
+                    }
+                    // return done(null, dbModel);
+                    // res.send("Login successful!")
+                }
 
-                    }
-                    if (dbModel.length !== 0) {
-                        return done(null, dbModel);
-                        // res.send("Login successful!")
-                    } else {
-                        res.send("Login error!");
-                    }
-                })
-                .catch(err => {
-                    console.log("error", err);
-                    res.status(422).json(err)
-                });
+                // console.log("dbModel", dbModel);
+
+
+            })
+            .catch(err => {
+                console.log("error", err);
+                res.status(422).json(err)
+            });
     }
 }
